3f85b1718c0bb294b4d02ab3ac4af3e1d1607835
feklaKonstantinovna <f.konstantinovna@gmail.com>
2025-05-12 14:20:35 +0300

    add PageComponent

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -21,6 +21,11 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
+# Данная фикстура используется с открытием уже сохраненного состояния
+@pytest.fixture()
+def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page_with_state)
+
 @pytest.fixture()
 def courses_page_list(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
