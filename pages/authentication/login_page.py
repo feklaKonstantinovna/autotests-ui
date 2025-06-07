d75b9c4eba357a62bd806186b40f1e6be46c3a98
feklaKonstantinovna <f.konstantinovna@gmail.com>
2025-06-07 14:11:59 +0300

    Homework: Introduction to PageFactory

--- pages/authentication/login_page.py
+++ pages/authentication/login_page.py
@@ -26,6 +26,3 @@ class LoginPage(BasePage):
     def check_visible_wrong_email_or_password_alert(self):
         self.wrong_email_or_password_alert.check_visible()
         self.wrong_email_or_password_alert.check_have_text('Wrong email or password')
-
-        # expect(self.wrong_email_or_password_alert).to_be_visible()
-        # expect(self.wrong_email_or_password_alert).to_have_text('Wrong email or password')
